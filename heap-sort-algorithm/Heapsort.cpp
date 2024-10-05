cee58a2a910168b1f798d7330a55e9bd92fc8bf6
Dodas_ <ph005009004@gmail.com>
2024-10-05 10:11:34 -0300

    feat:torna o tamanho do array dinâmico
    Criei um define com o nome de LEN (LENGTH) que serve para controlar de forma simples o tamanho do array (ou árvore binária).

--- heap-sort-algorithm/Heapsort.cpp
+++ heap-sort-algorithm/Heapsort.cpp
@@ -2,33 +2,37 @@
 #include <cstdlib> // Para rand() e srand()
 #include <ctime>   // Para time()
 
+#define LEN 8 // Tamanho do array
+
 int aux=0;
 
 void printBinaryTree(int *binaryTree) {
-    for(int k = 0; k < 8; k++) {
-        if (k == 7) {
+    for(int k = 0; k < LEN; k++) {
+        if (k == LEN - 1) {
             printf("%d\n", binaryTree[k]);
             return;
         }
-        
         printf("%d ", binaryTree[k]);
     }
 }
 
 void MIN_HEAP (int *binaryTree) {
-    
+    /* 
+     * Código desenvolvido pelo grupo, seguindo a lógica MIN_HEAP
+     */
+
     int change = 0;
     do {
         change = 0;
-        for(int j = 0; j < 8; j++) {
-            if(binaryTree[2*j+1] <= binaryTree[j] && 2*j+1 < 8) {
+        for(int j = 0; j < LEN; j++) {
+            if(binaryTree[2*j+1] <= binaryTree[j] && 2*j+1 < LEN) {
                 aux = binaryTree[2*j+1];
                 binaryTree[2*j+1] = binaryTree[j];
                 binaryTree[j] = aux;
                 change = 1;
             }
 
-            if(binaryTree[2*j+2] <= binaryTree[j] && 2*j+2 < 8) {
+            if(binaryTree[2*j+2] <= binaryTree[j] && 2*j+2 < LEN) {
                 aux = binaryTree[2*j+2];
                 binaryTree[2*j+2] = binaryTree[j];
                 binaryTree[j] = aux;
@@ -43,11 +47,11 @@ void MIN_HEAP (int *binaryTree) {
 int main() {
     srand(static_cast<unsigned int>(time(0)));
 
-    int binaryTree[8] = {3, 12, 9, 14, 10, 18, 8, 23};
+    int binaryTree[LEN] = {3, 12, 9, 14, 10, 18, 8, 23};
     //3, 12, 9, 14, 10, 18, 8, 23
     //43, 40, 55, 15, 84, 45, 71, 97
 
-    for(int i = 0; i < 8; i++) {
+    for(int i = 0; i < LEN; i++) {
         binaryTree[i] = rand() % 101;
     }
 
